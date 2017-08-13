bff17b2647ccd54ff337508e3c9e71338ee18a43
chun pin <james19840628@gmail.com>
2017-08-13 22:52:12 +0200

    add reactive-blueimp-gallery

---
+++ projs/batchfield-react/src/assets/galleryData.js
@@ -0,0 +1,245 @@
+const galleryData = [
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_00_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_01_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_02_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_03_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_04_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_05_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_06_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_07_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_08_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_09_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_10_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_11_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_12_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_13_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_14_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_15_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_16_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_17_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_18_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_19_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_20_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_21_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_22_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_23_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_24_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_25_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_26_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_27_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_28_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_29_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_30_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_31_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_32_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_33_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_34_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_35_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_36_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_37_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_38_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_39_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_40_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_41_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_42_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_43_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_44_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_45_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_46_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_47_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_48_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_49_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_50_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_51_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_52_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_53_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_54_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_55_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_56_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_57_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_58_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_59_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_60_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_61_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_62_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_63_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_64_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_65_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_66_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_67_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_68_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_69_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_70_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_71_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_72_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_73_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_74_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_75_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_76_350px.jpg'
+}, 
+{
+  source: '/images/happiness_foto/batchfield_fotograf_rosenheim_77_350px.jpg'
+}
+];
+
+
+export default galleryData;
+
+
+
+
+
+
--- projs/batchfield-react/src/pages/homePage.js
+++ projs/batchfield-react/src/pages/homePage.js
@@ -3,20 +3,44 @@ import FotoShootings from '../components/fotoShootings.js';
 import LandingCarousel from '../components/landingCarousel.js';
 import ServiceIntro from '../components/serviceIntro.js';
 
+import ReactGallery from 'reactive-blueimp-gallery';
+
 import data from '../assets/data.js';
+import galleryData from '../assets/galleryData.js';
+
+
+
 
 
 
 class HomePage extends Component {
+ //  test(){
+  	
+ //  	var galleryData = [];
+	// for (var i = 0 ; i < 78; i++){
+	// 	galleryData.push(`{source: '/images/happiness_foto/batchfield_fotograf_rosenheim_${(i < 10 ) ? '0'+i : i}_350px.jpg'}`);
+	// }
+
+	// console.log(galleryData);
+
+ //  }
+
   render() {
     return (
       <div>
         <LandingCarousel />
         <FotoShootings/>
         <ServiceIntro data={data.service_intro}/>
+ 
+        <ReactGallery>
+		  { galleryData.map(item => <ReactGallery.Slide { ...item } key={ item.source } />) }
+		</ReactGallery>
+		
       </div>
     );
   }
 }
 
 export default HomePage;
+
+
