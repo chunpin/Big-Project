b319fe10548903dac55164d7266e7a5b0abcd318
chun pin <james19840628@gmail.com>
2017-08-17 20:48:13 +0200

    get rid of react-slick, and add react-own-carousel2

--- projs/batchfield-react/src/components/promotionCarousel.js
+++ projs/batchfield-react/src/components/promotionCarousel.js
@@ -1,11 +1,12 @@
 
 import React, { Component } from 'react';
 
-import  Slider from 'react-slick';
 
 
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import OwlCarousel from 'react-owl-carousel2';
+
+
+import 'react-owl-carousel2/src/owl.carousel.css';
 import './promotionCarousel.css';
 
 
@@ -13,61 +14,48 @@ import './promotionCarousel.css';
 import bewebungsbilderPromotion01 from '../assets/images/batchfield_fotograf_rosenheim_bewebungsbilder_promotion-02.jpg';
 
 
-
+const options = {
+    items: 1,
+    nav: true,
+    rewind: true,
+    autoplay: true,
+    autoplayHoverPause:true,
+    center:true,
+    lazyLoad:true,
+    animateOut:true,
+    loop:true, //Infinity loop. Duplicate last and first items to get loop illusion.
+    nav:true,
+    navText: [
+              '<i class="fa fa-chevron-left"></i>',
+              '<i class="fa fa-chevron-right"></i>'
+              ],
+    responsive:{
+        640:{
+            items:1.4
+        },
+        1300:{
+            items:1.8
+        },
+        1700:{
+            items:2
+        }
+    }
+};
+ 
 
 
 class PromotionCarousel extends Component {
 
 	render(){
-		var settings = {
-		  infinite: true,
-	      infinite: true,
-	      speed: 500,
-	      // centerMode:true,
-	      // adaptiveHeight:true,
-	      slidesToShow: 2,
-		  slidesToScroll: 2,
-	      responsive: [
-		    {
-		      breakpoint: 3000,
-		      settings: {
-		        slidesToShow: 3,
-		        slidesToScroll: 3,
-		        infinite: true,
-		        dots: true,
-		        centerMode:true
-		      }
-		    },
-		    {
-		      breakpoint: 600,
-		      settings: {
-		        slidesToShow: 1,
-		        slidesToScroll: 1,
-		        centerMode:false
-		      }
-		    }
-		    // You can unslick at a given breakpoint now by adding:
-		    // settings: "unslick"
-		    // instead of a settings object
-		  ]
-
-	    };
-
 		return (
 		   <section>
 		   	<h1 className="title">Aktionen</h1>
-
-		   	<Slider {...settings}>
-		   	{/*
-		   	    <div><h3>1</h3></div>
-		   	    <div><h3>2</h3></div>
-		   	    <div><h3>3</h3></div>
-		   		 */}
-		        <div><a className="promotion-link" href="#"><img src={bewebungsbilderPromotion01} alt="bewebungsbilderPormotion"/></a></div>
-		        <div><a className="promotion-link" href="#"><img src={bewebungsbilderPromotion01} alt="bewebungsbilderPormotion"/></a></div>
-		        <div><a className="promotion-link" href="#"><img src={bewebungsbilderPromotion01} alt="bewebungsbilderPormotion"/></a></div>
-		       
-	      	</Slider>
+		   	<OwlCarousel ref="car" options={options}>
+			    <div><img src={bewebungsbilderPromotion01 } alt="The Last of us"/></div>
+			    <div><img src={bewebungsbilderPromotion01 } alt="GTA V"/></div>
+			    <div><img src={bewebungsbilderPromotion01 } alt="Mirror Edge"/></div>
+			</OwlCarousel>
+		
 		   </section>
 		   
 		);
@@ -77,6 +65,9 @@ class PromotionCarousel extends Component {
 
 
 export default PromotionCarousel;
+
+
+// <OwlCarousel ref="car" options={options} events={events} >
 
 
 
